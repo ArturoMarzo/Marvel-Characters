3c1198d7810ac535db3718ace13344cddb1e3376
Rollling Asphalt <marzodelafuente@gmail.com>
2020-03-04 21:54:37 +0100

    more comments

--- MarvelCharacters/Presentation/Modules/CharactersList/CharactersListContract.swift
+++ MarvelCharacters/Presentation/Modules/CharactersList/CharactersListContract.swift
@@ -3,7 +3,7 @@ import UIKit
 
 /*
 By defining a contract it is easier to see the relationship between the
-presenter, the view, the router... in VIPER It also makes easier to test the different layers
+presenter, the view, the router... in VIPER. It also makes easier to test the different layers
 */
 
 protocol CharactersListBuilder {
